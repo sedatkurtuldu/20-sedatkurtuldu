f0b329c50cfc934c5bb7671c21bde0d79e4109a7
sedatkurtuldu <115918615+sedatkurtuldu@users.noreply.github.com>
2024-01-09 23:43:31 +0300

    inital commit

--- App.js
+++ App.js
@@ -1,12 +1,33 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useEffect, useState } from 'react';
+import { FlatList, StyleSheet, Text, View } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
 
 export default function App() {
+
+  const [users, setUsers] = useState([])
+
+  useEffect(() => {
+    fetch('http://localhost:3001/users')
+      .then(response => response.json())
+      .then(json => console.log(json))
+  }, [])
+  
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <SafeAreaView>
+       <FlatList
+        data={users.users}
+        keyExtractor={(item) => item.id}
+        renderItem={({ item }) => (
+          <View>
+            <Text>Username: {item.username}</Text>
+            <Text>Password: {item.password}</Text>
+          </View>
+        )}
+      />
       <StatusBar style="auto" />
-    </View>
+    </SafeAreaView>
   );
 }
 
