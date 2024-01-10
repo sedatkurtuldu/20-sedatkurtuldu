6c65d1a74c28e2a4ccc9ec1e4119d8f1e0e7af9d
sedatkurtuldu <115918615+sedatkurtuldu@users.noreply.github.com>
2024-01-11 00:23:46 +0300

    All tests has passed.

--- src/mocks/handlers.ts
+++ src/mocks/handlers.ts
@@ -7,7 +7,11 @@ export const handlers = [
 
   http.get(`${getBaseUrl()}/users`, async () => {
     
-    const users =  await getUsers();
+    const users =  [
+      {"id":"diZqYcp7jZFn8qUgOalE","userName":"Yaren","password":"12345678"},
+      {"id":"mfU12DAF67QIooMG9Et0","userName":"Cankat","password":"1234567"},
+      {"id":"roCGOruDxAf1Aa9CXaJi","userName":"Sedat","password":"123456"}
+    ];
 
     return HttpResponse.json(users, { status: 200 });
   }),
